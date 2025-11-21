81d1cc9841c36c22cbac8ea8f8ab8077b2f28387
Roha45 <rohanmk2006@gmail.com>
2025-11-21 12:16:18 +0530

    RAG(Retrieval-Augmented Generation)

--- first.js
+++ first.js
@@ -1,134 +1,51 @@
-import { GoogleGenAI } from "@google/genai";
-import readlineSync from 'readline-sync';
-import { exec } from "child_process";
-import { promisify } from "util";
-import os from 'os'
+import * as dotenv from 'dotenv';
+dotenv.config();
+import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf';
+import { RecursiveCharacterTextSplitter } from '@langchain/textsplitters';
+import { GoogleGenerativeAIEmbeddings } from '@langchain/google-genai';
+import { Pinecone } from '@pinecone-database/pinecone';
+import { PineconeStore } from '@langchain/pinecone';
 
-const platform = os.platform();
+async function indexDocument() {
+    // Pdf ko lode karne ka
+    const PDF_PATH = './dsa.pdf';
+    const pdfLoader = new PDFLoader(PDF_PATH);
+    const rawDocs = await pdfLoader.load();  
+    
+    console.log("pdf loaded");
 
-const asyncExecute = promisify(exec);
+    //chanking karo
+    const textSplitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 1000,
+        chunkOverlap: 200,
+    });
+    const chunkedDocs = await textSplitter.splitDocuments(rawDocs);
 
-const History = [];
-const ai = new GoogleGenAI({ apiKey: "AIzaSyA5RH2aQzkSxdXNDoG9EPQTaceAxsinIdM"});
+    console.log("chunck created");
 
+    // vector embedding module
+    const embeddings = new GoogleGenerativeAIEmbeddings({
+        apiKey: process.env.GEMINI_API_KEY,
+        model: 'text-embedding-004',
+    });
 
-async function executeCommand({command}) {
-    try{
-        const {stdout,stderr}= await asyncExecute(command);
+    console.log("embedding configure");
 
-        if(stderr){
-            return `Error: ${stderr}`;
-        }
-        return `Success: ${stdout} || Task executed completely`;
-    }
-    catch(error){
-        return `Error: ${error}`;
-    }
+    //database ko configure kar lete hai
+    //Initialize Pinecone Client
+    const pinecone = new Pinecone();
+    const pineconeIndex = pinecone.Index(process.env.PINECONE_INDEX_NAME);
+
+    console.log("pinecone configure");
+
+    //langchain (chainking,embedding,database);
+    await PineconeStore.fromDocuments(chunkedDocs, embeddings, {
+        pineconeIndex,
+        maxConcurrency: 5,
+    });
+
+    console.log("data store succesfuly");
+
 }
 
-const executeCommandDeclaration={
-    name:'executeCommand',
-    description:"Execute a single terminal/shell command. A command can be to create a folder, file, write on a file, edit the file or delete the file",
-    parameters:{
-        type:'OBJECT',
-        properties:{
-            command:{
-                type:'STRING',
-                description: 'It will be a single terminal command. Ex: "mkdir calculator'
-            },
-        },
-        required: ['command']
-    }
-}
-
-const availableTools = {
-    executeCommand
-}
-
-async function runAgent(userProblem) {
-
-    History.push({
-        role:"user",
-        parts:[{text:userProblem}]
-    })
-    while(true){
-        const response=await ai.models.generateContent({
-            model:"gemini-2.5-flash",
-            contents: History,
-            config:{
-                systemInstruction: `You are an Website builder expert. You have to create the frontend of the website by analysing the user Input.
-        You have access of tool, which can run or execute any shell or terminal command.
-        
-        Current user operation system is: ${platform}
-        Give command to the user according to its operating system support.
-
-
-        <-- What is your job -->
-        1: Analyse the user query to see what type of website the want to build
-        2: Give them command one by one , step by step
-        3: Use available tool executeCommand
-
-        // Now you can give them command in following below
-        1: First create a folder, Ex: mkdir "calulator"
-        2: Inside the folder, create index.html , Ex: touch "calculator/index.html"
-        3: Then create style.css same as above
-        4: Then create script.js
-        5: Then write a code in html file
-
-        You have to provide the terminal or shell command to user, they will directly execute it
-`,
-
-                tools: [{
-                    functionDeclarations: [executeCommandDeclaration]
-                }],
-            }
-        })
-
-        if(response.functionCalls && response.functionCalls.length>0){
-            const {name,args}=response.functionCalls[0];
-            const funCall =  availableTools[name];
-            const result = await funCall(args);
-
-            const functionResponsePart = {
-                name: name,
-                response: {
-                    result: result,
-                }
-            };
-            History.push({
-                role: "model",
-                parts: [
-                    {
-                    functionCall: response.functionCalls[0],
-                    },
-                ],
-            });
-
-            // result Ko history daalna
-
-            History.push({
-                role: "user",
-                parts: [
-                    {
-                    functionResponse: functionResponsePart,
-                    },
-                ],
-            });
-        }else{
-            History.push({
-                role:'model',
-                parts:[{text:response.text}]
-            })
-            console.log(response.text);
-            break;
-        }
-    }    
-}
-
-async function main() {
-    const userProblem = readlineSync.question("Ask me anything--> ");
-    await runAgent(userProblem);
-    main();
-}
-
-main();
+indexDocument();
